e08eb3a0a7d111ea9c0b650700f03c1831940f29
dgusfr <diegogustavo.fr@gmail.com>
2024-10-23 09:05:52 -0300

    feat(sum)

--- Arrays/1.js
+++ Arrays/1.js
@@ -1,10 +1,10 @@
 function somaArray(numeros) {
   let soma = 0;
-  numeros.forEach((numero)  {
+  numeros.forEach((numero) => {
     soma += numero;
   });
   return soma;
 }
 
 const arrayDeNumeros = [1, 2, 3, 4, 5];
-console.log(somaArray(arrayDeNumeros)); // Saída: 15
+console.log(somaArray(arrayDeNumeros));
