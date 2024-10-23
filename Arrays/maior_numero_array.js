f13c9b8a25cca4728612eef9a45a72ab5c49e1fa
dgusfr <diegogustavo.fr@gmail.com>
2024-10-23 09:14:16 -0300

    desestructuring array

--- Arrays/maior_numero_array.js
+++ Arrays/maior_numero_array.js
@@ -1,21 +1,3 @@
-function encontraMaiorNumero(array) {
-  if (array.length == 0) {
-    console.log("A array está vazia");
-  }
-  let maior = array[0];
-  array.forEach((numero) => {
-    if (numero > maior) {
-      maior = numero;
-    }
-  });
-  return maior;
-}
-
-const numeros = [10, 5, 20, 8, 15];
-console.log(encontraMaiorNumero(numeros));
-
-//**********Ou usando Math************//
-
 function encontrarMaior(array) {
   if (array.length === 0) {
     return "A array está vazia.";
@@ -23,5 +5,5 @@ function encontrarMaior(array) {
   return Math.max(...array);
 }
 
-const algunsNumeros = [10, 5, 20, 8, 15];
+const algunsNumeros = [10, 5, 20, 48, 15];
 console.log(encontrarMaior(algunsNumeros));
