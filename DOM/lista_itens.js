5131ad57d44d5b5dc3c211c31d98308c16de7986
dgusfr <diegogustavo.fr@gmail.com>
2024-10-24 11:10:52 -0300

    feat(struct)

--- DOM/lista_itens.js
+++ DOM/lista_itens.js
@@ -1,5 +1,3 @@
-// Implemente uma função que cria uma lista de itens e a adiciona ao DOM.
-// Utilize createElement() para criar os elementos da lista. Utilize appendChild() para adicionar os elementos à lista.
 function criarLista() {
   const lista = document.createElement("ul");
   for (let i = 1; i <= 5; i++) {
