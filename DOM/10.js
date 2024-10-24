fc4ead9c3914860e76253bf4db3f9913e5ee5ca1
dgusfr <diegogustavo.fr@gmail.com>
2024-10-24 11:11:58 -0300

    feat(struct)

--- DOM/10.js
+++ DOM/10.js
@@ -1,5 +1,3 @@
-// Implemente uma função que busca e retorna todos os links de um documento.
-// Utilize getElementsByTagName("") para selecionar todos os elementos de âncora. Percorra a lista de elementos e extraia os links.
 function buscarLinks() {
   const links = [];
   const elementos = document.getElementsByTagName("a");
