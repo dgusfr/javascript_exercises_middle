eb621e2836423a85788d7e8ebe49ba3b2b1dadb8
dgusfr <diegogustavo.fr@gmail.com>
2024-10-24 11:10:23 -0300

    feat(struct)

--- DOM/6.js
+++ DOM/6.js
@@ -1,4 +1,2 @@
-// Desenvolva um programa que insere um novo elemento HTML após outro.
-// Utilize querySelector("") para selecionar o elemento de referência. Utilize insertAdjacentHTML() para inserir o novo elemento.
 const elementoReferencia = document.querySelector(".referencia");
 elementoReferencia.insertAdjacentHTML("afterend", "<div>Novo Elemento</div>");
