bc28a31c3621b8871012eae7e2f8072cab2f55a2
dgusfr <diegogustavo.fr@gmail.com>
2024-10-24 11:08:46 -0300

    feat(struct)

--- DOM/3.js
+++
@@ -1,4 +0,0 @@
-// Desenvolva um programa que adiciona uma classe CSS a um elemento.
-// Utilize querySelector("") para selecionar o elemento. Utilize classList.add("") para adicionar a classe.
-const elemento = document.querySelector(".classe");
-elemento.classList.add("novaClasse");
---
+++ DOM/adiciona_classe.js
@@ -0,0 +1,2 @@
+const elemento = document.querySelector(".classe");
+elemento.classList.add("novaClasse");
