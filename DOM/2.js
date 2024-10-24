95653c2207f6b814e4cc2697b9e92dcea80e59a4
dgusfr <diegogustavo.fr@gmail.com>
2024-10-24 11:08:14 -0300

    feat(struct)

--- DOM/2.js
+++
@@ -1,5 +0,0 @@
-// Crie uma função que altera o texto de um elemento HTML.
-// Utilize getElementById("") para selecionar o elemento pelo ID. Utilize innerText para modificar o texto do elemento.
-function alterarTexto() {
-  document.getElementById("elemento").innerText = "Novo texto";
-}
